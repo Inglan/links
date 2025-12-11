392189043878a360f5a30517f144697fba79ca63
Ingo Wolf <me@ingowolf.au>
2025-12-11 20:04:15 +1100

    add create link section

---
+++ components/create-link.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Kbd } from "./ui/kbd";
+
+export default function CreateLink() {
+  return (
+    <div className="border p-2 flex flex-col gap-2">
+      <div>create</div>
+      <div className="flex flex-row gap-2">
+        <Input />
+        <Input />
+        <Button variant="outline">
+          Create<Kbd>⏎</Kbd>
+        </Button>
+      </div>
+    </div>
+  );
+}
