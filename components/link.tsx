1a7e79870f285adfe8ece2b72deba135c847d106
Ingo Wolf <me@ingowolf.au>
2025-12-12 12:28:27 +1100

    add delete button

--- components/link.tsx
+++ components/link.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { Link } from "@/lib/types";
+import { Button } from "./ui/button";
+import { Trash } from "lucide-react";
 
 export default function LinkComponent({ link }: { link: Link }) {
   return (
-    <div className="p-2 border">
-      <h2 className="text-lg">/{link.path}</h2>
-      <h3 className="text-sm text-neutral-400">{link.link}</h3>
+    <div className="p-2 border flex flex-row">
+      <div className="flex flex-col grow">
+        <h2 className="text-lg">/{link.path}</h2>
+        <h3 className="text-sm text-neutral-400">{link.link}</h3>
+      </div>
+      <Button variant="ghost" size="icon" onClick={() => console.log("Delete")}>
+        <Trash />
+      </Button>
     </div>
   );
 }
