f9e872dc620822a8a3e72b1a426d5dca08980547
Ingo Wolf <me@ingowolf.au>
2025-12-12 12:30:48 +1100

    update links component

--- components/link.tsx
+++ components/link.tsx
@@ -2,18 +2,30 @@
 
 import { Link } from "@/lib/types";
 import { Button } from "./ui/button";
-import { Trash } from "lucide-react";
+import { Pencil, Trash } from "lucide-react";
 
 export default function LinkComponent({ link }: { link: Link }) {
   return (
-    <div className="p-2 border flex flex-row">
+    <div className="border flex flex-row p-2 items-center">
       <div className="flex flex-col grow">
         <h2 className="text-lg">/{link.path}</h2>
         <h3 className="text-sm text-neutral-400">{link.link}</h3>
+        <h3 className="text-sm text-neutral-400">
+          {new Date(link.createdAt).toLocaleString()}
+        </h3>
       </div>
-      <Button variant="ghost" size="icon" onClick={() => console.log("Delete")}>
-        <Trash />
-      </Button>
+      <div className="flex flex-col">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => console.log("Delete")}
+        >
+          <Trash />
+        </Button>
+        <Button variant="ghost" size="icon" onClick={() => console.log("Edit")}>
+          <Pencil />
+        </Button>
+      </div>
     </div>
   );
 }
