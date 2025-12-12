672495acc1ba90cf2509c7b83f1a9242c410faf9
Ingo Wolf <me@ingowolf.au>
2025-12-12 11:34:18 +1100

    add check to see if link exists

--- lib/links.ts
+++ lib/links.ts
@@ -3,6 +3,8 @@
 import { redis } from "./redis";
 
 export async function create(link: string, path: string) {
+  const existing = await redis.get(`link:${path}`);
+  if (existing) throw new Error("Link already exists");
   await redis.set(
     `link:${path}`,
     JSON.stringify({ link, createdAt: Date.now() }),
