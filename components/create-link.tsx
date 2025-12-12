e13e0fb4518829adee16eb984da5b1cd2fcd286b
Ingo Wolf <me@ingowolf.au>
2025-12-12 12:37:20 +1100

    chnage create box styling

--- components/create-link.tsx
+++ components/create-link.tsx
@@ -15,41 +15,43 @@ export default function CreateLink({
   const [pending, setPending] = useState(false);
 
   return (
-    <div className="border p-2 flex flex-col gap-2 col-span-2">
-      <div>create</div>
-      <form
-        className="flex flex-row gap-2"
-        onSubmit={async (event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-          const formData = new FormData(event.currentTarget);
+    <div className="md:col-span-2 py-4">
+      <div className="flex flex-col gap-2 max-w-md mx-auto">
+        <div>create</div>
+        <form
+          className="flex flex-row gap-2"
+          onSubmit={async (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
 
-          setPending(true);
-          try {
-            await links.create(
-              formData.get("link")?.toString() || "",
-              formData.get("path")?.toString() || "",
-            );
-            refetchAction();
-          } finally {
-            setPending(false);
-          }
+            setPending(true);
+            try {
+              await links.create(
+                formData.get("link")?.toString() || "",
+                formData.get("path")?.toString() || "",
+              );
+              refetchAction();
+            } finally {
+              setPending(false);
+            }
 
-          event.currentTarget.reset();
-        }}
-      >
-        <Input
-          type="url"
-          disabled={pending}
-          placeholder="link"
-          name="link"
-          required
-          autoFocus
-        />
-        <Input disabled={pending} placeholder="path" name="path" />
-        <Button variant="outline" disabled={pending}>
-          create<Kbd>⏎</Kbd>
-        </Button>
-      </form>
+            event.currentTarget.reset();
+          }}
+        >
+          <Input
+            type="url"
+            disabled={pending}
+            placeholder="link"
+            name="link"
+            required
+            autoFocus
+          />
+          <Input disabled={pending} placeholder="path" name="path" />
+          <Button variant="outline" disabled={pending}>
+            create<Kbd>⏎</Kbd>
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
