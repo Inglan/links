77cf317c9557efa42b483b8a010653f4f53317ad
Ingo Wolf <me@ingowolf.au>
2025-12-12 11:12:42 +1100

    switch to just profile picture

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,7 @@
 import { auth, signOut } from "@/auth";
 import CreateLink from "@/components/create-link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function AdminPage() {
@@ -10,9 +11,20 @@ export default async function AdminPage() {
     return (
       <div className="p-2">
         <div className="w-full max-w-xl mx-auto flex flex-col gap-2">
-          <div className="flex flex-row">
-            <h1 className="text-xl">links</h1>
+          <div className="flex flex-row gap-2">
+            <Button size="sm">links</Button>
             <div className="grow"></div>
+            <Button size="sm" variant="ghost"></Button>
+            <Image
+              src={session.user?.image || ""}
+              alt={session.user?.name || ""}
+              width={32}
+              height={32}
+              className="border"
+            />
+            {/*<Button size="sm" variant="outline">
+              {session.user?.name}
+            </Button>
             <form
               action={async () => {
                 "use server";
@@ -22,7 +34,7 @@ export default async function AdminPage() {
               <Button variant="outline" size="sm" type="submit">
                 logout
               </Button>
-            </form>
+            </form>*/}
           </div>
           <CreateLink />
         </div>
